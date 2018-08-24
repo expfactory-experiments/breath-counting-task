645731c1290e5730d8df44a40db304504f558472
Paul Sharpe <paul@miraclefish.com>
2018-08-24 15:08:26 +0100

    FIX: Rename R package.
    See https://github.com/expfactory/expfactoryr/issues/1.

--- efbreathr/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(efbreathr)
-
-test_check("efbreathr")
--- expfactory.breathcounting/tests/testthat/test_breath_counting.R
+++ expfactory.breathcounting/tests/testthat/test_breath_counting.R
@@ -2,11 +2,11 @@ library(tidyverse)
 context('Breath counting')
 
 test_that("process_expfactory_bc_file() can process a CSV file", {
-  expect_silent(efbreathr::process_breath_counting('../fixtures/1/1/1_bc.csv', p=1, json=FALSE))
+  expect_silent(process_breath_counting('../fixtures/1/1/1_bc.csv', p=1, json=FALSE))
 })
 
 test_that("expfactory_breath_counting_to_csv() works", {
-  expect_silent(efbreathr::expfactory_breath_counting_to_csv('../fixtures/1', 1, c(2,6,7)))
+  expect_silent(expfactory_breath_counting_to_csv('../fixtures/1', 1, c(2,6,7)))
 })
 
 ## JSON data
@@ -15,9 +15,9 @@ json_p = data.frame(token = c('00b96573-0f35-4f17-88e4-2836ca7deec1','03e32ecf-8
 fixture_dir <- '../fixtures/2'
 path <- paste0(fixture_dir,'/',json_p[1,1],'_finished/breath-counting-task-results.json')
 test_that("process_breath_counting() can process a JSON file", {
-  expect_silent(efbreathr::process_breath_counting(path, p=1))
+  expect_silent(process_breath_counting(path, p=1))
 })
-bc <- efbreathr::process_breath_counting(path, p=1)
+bc <- process_breath_counting(path, p=1)
 test_that("process_breath_counting() parses data correctly", {
   expect_equal(bc[[1,'rt']],14085)
 })
@@ -25,14 +25,14 @@ test_that("process_breath_counting() parses data correctly", {
 bc <- expand.grid(token = json_p$token) %>%
   rowwise() %>%
   mutate(path=paste0(fixture_dir, '/', token, '_finished/breath-counting-task-results.json')) %>%
-  do(., efbreathr::process_breath_counting(.$path, .$token))
+  do(., process_breath_counting(.$path, .$token))
 
 df <- bc %>%
   filter(p == '00b96573-0f35-4f17-88e4-2836ca7deec1')
 test_that("breath_counting_accuracy() returns a data frame", {
-  expect_is(efbreathr::breath_counting_accuracy(df), "data.frame")
+  expect_is(breath_counting_accuracy(df), "data.frame")
 })
-foo <- efbreathr::breath_counting_accuracy(df)
+foo <- breath_counting_accuracy(df)
 test_that("breath_counting_accuracy() is accurate", {
   expect_equal(foo[[1,'correct']],12)
 })
